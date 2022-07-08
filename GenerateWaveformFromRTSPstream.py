fa1f3fc4a42c95e0cee72c48ccea0a0ec65d331c
Trung Kien <dtrungkin@gmail.com>
2022-07-08 16:48:34 +0700

    Updated faster ploting

--- GenerateWaveformFromRTSPstream.py
+++ GenerateWaveformFromRTSPstream.py
@@ -1,3 +1,4 @@
+import struct
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
@@ -5,8 +6,15 @@ from tkinter import TclError
 import time
 import ffmpeg
 from ffpyplayer.player import MediaPlayer
+
+from pydub import AudioSegment
+from pydub.playback import play
+import io
+import pyaudio
+
+
 # use this backend to display in separate Tk window
-matplotlib.use("TkAgg")
+matplotlib.use("QtAgg")
 
 # RTSP URL
 rtsp_stream = 'rtsp://localhost:8554/stream'
@@ -16,7 +24,7 @@ rtsp_stream = 'rtsp://localhost:8554/stream'
 
 # RTSP capture with ffmpeg
 stream = ffmpeg.input(rtsp_stream)
-audio = stream.audio
+audio = stream.audio 
 output_audio = (
     ffmpeg
     .output(audio, '-', acodec = 'pcm_s16le', ar = 44100, ac = 1, format = 's16le')
@@ -41,7 +49,7 @@ ax.set_xlabel('samples')
 ax.set_ylabel('amplitude')
 ax.set_ylim(-1.0, 1.0)
 ax.set_xlim(0.0, 2.0 * CHUNK)
-plt.setp(ax, xticks=[0*1.0, 1.0*CHUNK, 2.0 * CHUNK], yticks=[-1, -0.5, 0.0, 0.5, 1.0])
+plt.setp(ax, xticks=[0*1.0, 1.0*CHUNK, 2.0 * CHUNK], yticks=[-1.0, 0.0, 1.0])
 
 # Show the plot
 plt.show(block=False)
@@ -50,25 +58,35 @@ plt.show(block=False)
 frame_count = 0
 start_time = time.time()
 # player = MediaPlayer(rtsp_stream)
+# p = pyaudio.PyAudio()
+# audio_playback = p.open(format=pyaudio.paFloat32,
+#                     channels=1,
+#                     rate= 44100,
+#                     output=True
+# )
+# audio_playback.start_stream()
+
+# Initial ploting
+fig.canvas.draw()
 while True:
     # # Playback audio 
     # audio_framem, val = player.get_frame() 
     
     # Bytes data
     data = output_audio.stdout.read(CHUNK * 2)
-
     # Put data into a numpy array and convert to int16
     data_np = np.frombuffer(data, dtype=np.int16)
-
     # Convert data to float
     data_np = data_np.astype('float32') / 32767.0
-    # print(data_np)
+
     # Plot data
     line.set_ydata(data_np)
     try:
-        fig.canvas.draw()
+        ax.draw_artist(ax.patch)
+        ax.draw_artist(line)
+        fig.canvas.update()
         fig.canvas.flush_events()
-        frame_count += 1
+        # audio_playback.write(data_np)
     except TclError:
         frame_rate = frame_count / (time.time() - start_time)
         print('stream stopped')
