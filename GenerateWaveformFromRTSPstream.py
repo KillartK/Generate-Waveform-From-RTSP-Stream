2b0194ecefaddd9a39138978cd2df2f1816bcdc6
Trung Kien <dtrungkin@gmail.com>
2022-07-21 09:14:21 +0700

    Clean up

--- GenerateWaveformFromRTSPstream.py
+++ GenerateWaveformFromRTSPstream.py
@@ -1,5 +1,3 @@
-import struct
-from cv2 import threshold
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
@@ -8,19 +6,13 @@ import time
 import ffmpeg
 from ffpyplayer.player import MediaPlayer
 import multiprocessing
-from pydub import AudioSegment
 from pydub.playback import play
-import io
-import pyaudio
 
 # use this backend to display in separate window
 matplotlib.use("tkagg")
 
-# RTSP URL
-rtsp_stream = 'rtsp://localhost:8554/stream'
-
-# Playback audio with ffplayer
-# player = MediaPlayer(rtsp_stream)
+# RTSP stream input
+rtsp_stream = input('Enter your rtsp stream: ')
 
 # Samples per frame
 CHUNK = 2048
@@ -40,28 +32,23 @@ ax.set_xlabel('samples')
 ax.set_ylabel('amplitude')
 ax.set_ylim(-1.0, 1.0)
 ax.set_xlim(0.0, 2.0 * CHUNK)
+# Draw the threshold line
 threshold_positive = 0.25
 threshold_negative = -0.25
 ax.axhline(y=threshold_positive,linewidth=1, color='k', linestyle = 'dashed')
 ax.axhline(y=threshold_negative,linewidth=1, color='k', linestyle = 'dashed')
 plt.setp(ax, xticks=[0*1.0, 1.0*CHUNK, 2.0 * CHUNK], yticks=[-1.0, -0.5, threshold_negative, 0.0, threshold_positive, 0.5, 1.0])
-
 # Show the plot
 plt.show(block=False)
 
-# for measuring frame rate
-
-# p = pyaudio.PyAudio()
-# player = MediaPlayer(rtsp_stream)
-# audio_playback = p.open(format=pyaudio.paFloat32,
-#                     channels=1,
-#                     rate= 44100,
-#                     output=True
-# )
+# Play the audio 
 def playAudio(rtsp_stream):
     player = MediaPlayer(rtsp_stream)
     return player
+
+# Capture rtsp stream and plot the waveform
 def plotWaveform(rtsp_stream):
+    # Capture RTSP stream
     stream = ffmpeg.input(rtsp_stream)
     audio = stream.audio 
     output_audio = (
@@ -69,12 +56,13 @@ def plotWaveform(rtsp_stream):
         .output(audio, '-', acodec = 'pcm_s16le', ar = 44100, ac = 1, format = 's16le')
         .run_async(pipe_stdout = True)
     )
+
+    # for measuring frame rate
     frame_count = 0
     start_time = time.time()
+
+    # Start Plotting
     while True:
-        # # Playback audio 
-        # audio_framem, val = player.get_frame() 
-        
         # Bytes data
         data = output_audio.stdout.read(CHUNK * 2)
         # Put data into a numpy array and convert to int16
@@ -86,12 +74,8 @@ def plotWaveform(rtsp_stream):
         time.sleep(0.015)
         line.set_ydata(data_np)
         try:
-            # ax.draw_artist(ax.patch)
-            # ax.draw_artist(line)
-            # fig.canvas.update()
             fig.canvas.draw()
             fig.canvas.flush_events()
-            # audio_playback.write(data_np)
         except TclError:
             frame_rate = frame_count / (time.time() - start_time)
             print('stream stopped')
@@ -101,7 +85,5 @@ def plotWaveform(rtsp_stream):
 # start 2 process at a same time
 first_process = multiprocessing.Process(name = 'p1', target=playAudio(rtsp_stream))
 second_process = multiprocessing.Process(name='p2', target=plotWaveform(rtsp_stream))
-
-
 first_process.start()
 second_process.start()
